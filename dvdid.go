47dfc3dcdc34cfc96e3891889cd7cca906d8f636
Simon Vandaele <havrest@gmail.com>
2020-04-03 23:42:49 +0200

    First package issue

---
+++ dvdid.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "log"
+    "github.com/havrest/go-dvdid/dvdid"
+    "os"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        log.Fatalf("Usage: %s <Volume path>", os.Args[0])
+    }
+
+    log.Printf("Analysing volume: %s", os.Args[1])
+
+    discId, err := dvdid.ComputeDVDId(os.Args[1])
+
+    if err != nil {
+        log.Fatalf("Err: %s", err.Error())
+    } else {
+        log.Printf("DiscId: %s", discId)
+    }
+}
